7af9acf4886d7ffcae431e9bebce89d0f03501cf
Harshit Srivastava <hs3511295@gmail.com>
2026-09-15 03:27:35 +0530

    Prepare backend for deployment

--- backend/app.py
+++ backend/app.py
@@ -297,7 +297,10 @@ def dataset_file(filename):
 
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+
     app.run(
-        debug=False,
-        port=5000
+        host="0.0.0.0",
+        port=port,
+        debug=False
     )
